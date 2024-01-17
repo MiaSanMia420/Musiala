b98410ab48545538d86444783bdc608d5f839a6c
MiaSanMia420 <133535420+MiaSanMia420@users.noreply.github.com>
2024-01-17 18:57:30 +0200

    Delete readfiles.py

--- readfiles.py
+++
@@ -1,8 +0,0 @@
-def read(filename):
-    results = []
-    with open(filename, 'r') as file:
-        for line in file:
-            nums = list(map(int, line.split()))
-            result = process(nums)
-            results.append(result)
-    return results
