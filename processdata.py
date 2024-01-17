f78cc788a4eee64c14ff27f79e0f2d4b6d9b6c31
MiaSanMia420 <133535420+MiaSanMia420@users.noreply.github.com>
2024-01-17 18:24:51 +0200

    Update processdata.py

--- processdata.py
+++ processdata.py
@@ -1,12 +1,12 @@
 def process(data):
-    odd_counts = []
-    no_odd_nums = []
+    a = []
+    b = []
     for num in data:
         str_num = str(num)
-        odd_count = sum(1 for digit in str_num if int(digit) % 2 != 0)
-        odd_counts.append(odd_count)
-        if odd_count == 0:
-            no_odd_nums.append(num)
-    avg = sum(no_odd_nums) // len(no_odd_nums) if no_odd_nums else 0
-    odd_counts.append(avg)
-    return odd_counts
+        imp = sum(1 for digit in str_num if int(digit) % 2 != 0)
+        a.append(imp)
+        if imp == 0:
+            b.append(num)
+    avg = sum(b) // len(b) if b else 0
+    a.append(avg)
+    return a
